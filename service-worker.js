86f282406f3a69847801de6b764d12a0c495b70d
jelly1beans <jerbethsantiago28@gmail.com>
2026-02-27 18:32:00 +0800

    h

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,6 @@
-const CACHE_NAME = 'todo-v1';
-const urlsToCache = [
-  '/index.html',
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
-  'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js',
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("install", function(e){
+  e.waitUntil("todo-cache".then(function(cache){
+    return cache.addAll(["index.html"]);
+  })
+);
 });
